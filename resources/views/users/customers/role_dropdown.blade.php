46625427a238d2b9f5eebb5821a6bb9e87572b4d
info@webethicssolutions.com <info@webethicssolutions.com>
2020-07-11 11:44:07 +0530

    update changes

--- resources/views/users/customers/role_dropdown.blade.php
+++
@@ -1,5 +0,0 @@
-<select name="role_id" id="role_id" class="form-control select2-single" data-width="100%">
-@foreach($roles as $key =>$role)
-<option value="{{$role->id}}">{{$role->title}}</option>
-@endforeach
-</select>
--- routes/web.php
+++ routes/web.php
@@ -26,35 +26,17 @@ Route::group(['prefix' => '','as' => 'user.' ,'namespace' => 'User','middleware'
 	Route::get('users',array('as'=>'ajax.pagination','uses'=>'UsersController@ajaxPagination'));
 	Route::post('users',array('as'=>'ajax.pagination','uses'=>'UsersController@ajaxPagination'));
 	
-	Route::get('customers',array('as'=>'ajax.pagination','uses'=>'UsersController@ajaxCustomerPagination'));
-	Route::post('customers',array('as'=>'ajax.pagination','uses'=>'UsersController@ajaxCustomerPagination'));
-	Route::post('customer/edit/{request_id}', 'UsersController@customer_edit'); //Edit User
-	Route::get('customer/show/{request_id}', 'UsersController@customer_show')->name('users.show');
 	
-	Route::get('guests/{id}',array('as'=>'ajax.pagination','uses'=>'UsersController@ajaxGuestPagination'));
-	Route::post('guests/{id}',array('as'=>'ajax.pagination','uses'=>'UsersController@ajaxGuestPagination'));
-	
-	Route::get('customers-listing/{id}',array('as'=>'ajax.pagination','uses'=>'UsersController@ajaxCustomerListingPagination'));
-	Route::post('customers-listing/{id}',array('as'=>'ajax.pagination','uses'=>'UsersController@ajaxCustomerListingPagination'));
-	Route::post('update-customer/{id}','CustomersController@customerUpdate');
 	Route::post('user/enable-disable',array('uses'=>'UsersController@enableDisableUser'));
 	Route::post('user/delete_user/{id}', 'UsersController@delete_user')->name('users.delete');
 	Route::post('user/delete_customer/{id}', 'CustomersController@delete_customer')->name('customer.delete');
 	
-	Route::get('user/create', 'UsersController@create')->name('users.create');
-	Route::get('user/print/{id}', 'UsersController@print_code')->name('users.print_code');
-	Route::post('user/store/{user_id}', 'UsersController@store');
-	Route::get('user/show/{request_id}', 'UsersController@show')->name('users.show');
 	
 	Route::post('user/edit/{request_id}', 'UsersController@edit'); //Edit User
 	Route::post('update-profile/{user_id}', 'UsersController@profileUpdate');//UPDATE USER
 	Route::post('user/roleDropdown', 'UsersController@roleDropdown');
 	
 	Route::get('account', 'UsersController@account');
-	Route::post('openCancelSubscriptionModal', 'UsersController@openCancelSubscriptionModal');  //openCancelSubscriptionModal
-	Route::post('cancel_subscription', 'UsersController@cancelSubscription');  //cancel_subscription
-	Route::post('openResumeSubscriptionModal', 'UsersController@openResumeSubscriptionModal');  //openCancelSubscriptionModal
-	Route::post('resume_subscription', 'UsersController@resumeSubscription');  //cancel_subscription
 	
 	Route::get('logout', 'UsersController@logout');
 	
@@ -62,18 +44,6 @@ Route::group(['prefix' => '','as' => 'user.' ,'namespace' => 'User','middleware'
 	//Route::delete('users/destroy', 'UsersController@massDestroy')->name('users.massDestroy');
 	
 	
-	// REPORT FROM ANALYST 
-	Route::get('business-users',array('as'=>'ajax.pagination','uses'=>'BusinessUsersController@ajaxPagination'));
-	Route::get('requests/report/{request_id}', 'RequestsReplyController@reportForm');
-	Route::post('report/complete', 'RequestsReplyController@completeReport');
-	Route::post('confirmModal', 'CommonController@confirmModal');
-	Route::post('delete/reportFile', 'RequestsReplyController@DeleteReportFile');
-    Route::post('analyst/report', 'RequestsReplyController@anlystReportSubmit');
-    Route::post('status/change', 'RequestsReplyController@changeStatus');
-    Route::post('request/showComment', 'RequestsReplyController@showComment');
-    Route::post('request/updateComment', 'RequestsReplyController@ModifyComment');
-    //Route::get('analysts/requestreply/{request_id}', 'RequestsReplyController@requestReply');
-	Route::post('send-notification/{user_id}', 'UsersController@sendEmailNotification');
 	
 	// Global Setting 
 	Route::get('settings',array('uses'=>'SettingsController@index'));
@@ -95,17 +65,6 @@ Route::group(['prefix' => '','as' => 'user.' ,'namespace' => 'User','middleware'
 	Route::get('email/edit/{template_id}',array('uses'=>'EmailController@email_template_edit'));
 	Route::post('email/update',array('uses'=>'EmailController@email_template_update'));
 	
-	
-	//Notifiactions
-	Route::get('notifications',array('uses'=>'NotificationsController@index'));
-	Route::post('getUnreadNotifications',array('uses'=>'NotificationsController@getUnreadNotifications'));
-	Route::post('getUnreadNotificationsCount',array('uses'=>'NotificationsController@getUnreadNotificationsCount'));
-	
-	
-	// AUDIT LOGS
-	Route::get('audits',array('as'=>'ajax.pagination','uses'=>'AuditsController@ajaxPagination'));
-	Route::post('audits',array('as'=>'ajax.pagination','uses'=>'AuditsController@ajaxPagination'));
-	Route::post('audit/showEventDetail',array('uses'=>'AuditsController@showEventDetail'));
 	
 	
 	Route::get('report/viewdownload/{report_id}/{attached_id}', 'RequestsController@viewDonwloadReport'); //Download Report
@@ -129,13 +88,6 @@ Route::post('user/calculateAge', 'User\UsersController@calculateAge');
 Route::post('user/verifiedAadhar', 'User\UsersController@verifiedAadhar');
 	
 	
-//Route::get('register/{plan}', 'Auth\RegisterController@showRegistrationForm')->name('register');
-/* Route::get('register', 'Auth\RegisterController@showRegistrationForm')->name('register');
-Route::post('register', 'Auth\RegisterController@showRegistrationForm')->name('register'); */
-Route::post('/checkemail', 'Auth\RegisterController@checkemail');
-Route::post('/subscription', 'SubscriptionController@create')->name('subscription.create');
-Route::get('cron_subscription_package_end', 'SubscriptionController@checkSubscriptionPackage');  //check-subscription-package-end
-
 Route::get('verify/account/{token}', 'User\UsersController@verifyAccount'); //VERIFY ACCOUNT
 
 
@@ -147,11 +99,6 @@ Route::post('password/reset', 'Auth\ResetPasswordController@reset')->name('passw
 Route::post('password/reset_new_user_password', 'Auth\ResetPasswordController@reset_new_user_password');
 
 Route::group(['prefix' => '','as' => 'user.' ,'namespace' => 'User'], function () {
-	
-Route::get('/customer-info/{user_id}', 'CustomersController@customer_info')->name('customer_info');
-Route::get('thankyou/{user_id}/{customer_name}', 'CustomersController@thankyou')->name('thankyou');
-Route::post('/customer-info/{user_id}', 'CustomersController@customer_create')->name('customer_create');
-Route::post('/submit-contact/', 'CustomersController@submit_contact')->name('submit_contact');
 });
 
 //Route::post('stripe/webhook', '\Laravel\Cashier\WebhookController@handleWebhook');
