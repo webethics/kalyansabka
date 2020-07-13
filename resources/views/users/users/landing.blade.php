55f2c052a249256bd661d5cb20dcd40fdd9ae26b
info@webethicssolutions.com <info@webethicssolutions.com>
2020-07-13 16:35:41 +0530

    push changes to live

--- resources/views/users/users/landing.blade.php
+++ resources/views/users/users/landing.blade.php
@@ -20,7 +20,7 @@
             <div class="container h-100">
                <div class="row h-100 align-items-center justify-content-center text-center">
                   <div class="col-lg-12 align-self-end">
-                     <h1 class="text-uppercase text-white">Coming Soon...</h1>
+                     <h1 class="text-uppercase ">Coming Soon...</h1>
                   </div>
                  
                </div>
