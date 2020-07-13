b4169fb0c72f8752070fc39aad927b1b58e24763
info@webethicssolutions.com <info@webethicssolutions.com>
2020-07-13 18:22:19 +0530

    push changes to live

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -8,7 +8,7 @@
                             
                             <span class="logo_image d-block mb-3"><a href="{{url('/')}}"><img src="{{asset('img/logo.png')}}"></a></span>
                             
-                            <h6 class="mb-2 register_title">{{ trans('global.buy_policy') }}</h6>
+                            <h5 class="mb-4 register_title">{{ trans('global.buy_policy') }}</h5>
 							@if(Session::has('message'))
 							<div class="alert alert-success">
 							{{ Session::get('message') }}
